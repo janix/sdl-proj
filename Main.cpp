1ba55364299bb69046d4d3869ed105ec3361fbe7
Janusz Janik <janusz.janik@hotmail.com>
2018-04-24 14:52:23 +0200

    second

--- Main.cpp
+++ Main.cpp
@@ -1,27 +1,85 @@
 #include "SDL.h"
+#include "stdio.h"
 
-int main(int argc, char *argv[])
+bool init();
+bool loadMedia();
+void close();
+
+SDL_Window* gWindow = NULL;
+SDL_Surface* gScreenSurface = NULL;
+SDL_Surface* gHelloWorld = NULL;
+
+int SCREEN_WIDTH = 1629;
+int SCREEN_HEIGHT = 2499;
+
+bool init()
 {
-  SDL_Init(SDL_INIT_VIDEO);
-
-  SDL_Window *window = SDL_CreateWindow(
-    "SDL2Test",
-    SDL_WINDOWPOS_UNDEFINED,
-    SDL_WINDOWPOS_UNDEFINED,
-    640,
-    480,
-    0
-  );
-
-  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-  SDL_RenderClear(renderer);
-  SDL_RenderPresent(renderer);
-
-  SDL_Delay(3000);
-
-  SDL_DestroyWindow(window);
-  SDL_Quit();
-
-  return 0;
+	bool success = true;
+	if(SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+		success = false;
+	}
+	else
+	{
+		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
+		{
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+			success = false;
+		}
+		else
+		{
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
+		}
+	}
+	return success;
 }
+
+bool loadMedia()
+{
+	bool success = true;
+	gHelloWorld = SDL_LoadBMP("../images/hello.bmp");
+	if(gHelloWorld == NULL)
+	{
+		printf("Unable to load image %s! SDL Error: %s\n", "../images/hello.bmp", SDL_GetError() );
+		success = false;
+	}
+	return success;
+}
+
+void close()
+{
+	SDL_FreeSurface(gHelloWorld);
+	gHelloWorld = NULL;
+
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+	
+	SDL_Quit();
+}
+
+int main(int argc, char* args[])
+{
+	if (!init())
+	{
+		printf("Failed to initialize!\n");
+	}
+	else
+	{
+		if (!loadMedia())
+		{
+			printf("Failed to load media!\n");
+		}
+		else
+		{
+			SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+			SDL_UpdateWindowSurface(gWindow);
+			SDL_Delay(2000);
+		}
+	}
+	close();
+
+	return 0;
+}
+
